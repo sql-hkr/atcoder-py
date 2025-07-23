8541f9b7dcb01242abea77d806eb0e90652b41a9
sql-hkr <sql.hkr@gmail.com>
2025-07-23 22:55:53 +0900

    Add solution for ABC405 A - Is it rated?

---
+++ abc400/abc405/a.py
@@ -0,0 +1,13 @@
+# A - Is it rated?
+R, X = map(int, input().split())
+if X == 1:
+    if 1600 <= R <= 2999:
+        print("Yes")
+    else:
+        print("No")
+else:
+    if 1200 <= R <= 2399:
+        print("Yes")
+    else:
+        print("No")
+
