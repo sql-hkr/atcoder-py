bcbbf6218d21ca9a45451255281d6bacb8786249
sql-hkr <sql.hkr@gmail.com>
2025-07-27 10:22:37 +0900

    Add solution for ABC402 problem B

---
+++ abc400/abc402/b.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+Q = int(input())
+q = deque()
+for _ in range(Q):
+    a, *b = map(int, input().split())
+    if a == 1:
+        q.append(b[0])
+    else:
+        print(q.popleft())
